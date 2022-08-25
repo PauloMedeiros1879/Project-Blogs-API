6b1f5a4243aeacf8f1edca6035e69c11e17c9407
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-08-25 15:16:51 -0400

    Feat: Adiciona o model Category com suas propriedades e respeitando o que foi solicitado e adiciona os arquivos vazios das outras duas models faltantes REQ07

---
+++ src/database/models/category.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'Category',
+    underscored: true,
+  });
+  return Category;
+};
