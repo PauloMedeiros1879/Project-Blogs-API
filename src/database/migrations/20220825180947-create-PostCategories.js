6208b8e02945803635b3d0c2c9c2b7ad2b58b0b2
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-08-25 14:53:48 -0400

    Feat: Adiciona os dados da tabela PostCategories REQ01

--- src/database/migrations/20220825180947-create-PostCategories.js
+++ src/database/migrations/20220825180947-create-PostCategories.js
@@ -1,21 +1,36 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('PostCategories', {
+      
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey:true,
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        references: {
+          model:"BlogPosts",
+          key: "id",
+        }
+      },
+
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey:true,
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        references: {
+          model:"Categories",
+          key: "id",
+        }
+      },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: (queryInterface) => {
+    return queryInterface.dropTable('PostCategories');
+  },
 };
