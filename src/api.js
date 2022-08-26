f1e7f1167877c7b771bfd753d7f7df0810d0630f
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-08-26 18:18:59 -0400

    Fix: Erro na aplicação, tentando resolver REQ03

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const errorMiddleware = require('./middlewares/error');
 const loginRouter = require('./route/loginRoute');
 // ...
 
@@ -9,8 +8,6 @@ app.use(express.json());
 
 // ...
 app.use('/login', loginRouter);
-
-app.use(errorMiddleware);
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
