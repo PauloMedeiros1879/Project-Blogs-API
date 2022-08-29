ace566dc200c8feea09647724ecc81836b7430c8
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-08-29 12:35:18 -0400

    Feat: Adiciona tratamento de erro para solucionar o problema do REQ03

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const loginRouter = require('./route/loginRoute');
+const error = require('./middlewares/error');
 // ...
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.json());
 
 // ...
 app.use('/login', loginRouter);
+
+app.use(error);
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
