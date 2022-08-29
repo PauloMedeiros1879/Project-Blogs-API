af2df4be4d9439260b84223607d924b9387adef3
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-08-29 13:40:14 -0400

    Feat: Adiciona a camada controller, manipulando cada dado passado pelo service, retornando response com o status solicitado ou um erro REQ04

---
+++ src/controllers/userController.js
@@ -0,0 +1,43 @@
+const userService = require('../services/userService');
+
+const userController = {
+  create: async (req, res, next) => {
+    try {
+      const token = await userService.create(req.body);
+      return res.status(201).json(token);
+    } catch (e) {
+      return next(e);
+    }
+  },
+
+  findAll: async (_req, res, next) => {
+    try {
+      const users = await userService.findAll();
+      return res.status(200).json(users);
+    } catch (e) {
+      return next(e);
+    }
+  },
+
+  findOnUser: async (req, res, next) => {
+    try {
+      const user = await userService.findOneUser(req.params.id);
+      if (user.code) return next(user);
+      return res.status(200).json(user);
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  delete: async (req, res, next) => {
+    try {
+      const { id } = req.userData;
+      await userService.delete(id);
+      return res.status(204).end();
+    } catch (e) {
+      return next(e);
+    }
+  },
+};
+
+module.exports = userController;
